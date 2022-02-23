60082816ff248283547fbb343d842a2343e8f204
Gaurav-Kumar-newspark <39953246+Gaurav-Kumar-newspark@users.noreply.github.com>
2022-02-23 11:10:47 +0530

    add hook to remove System URL
    This hook is added to remove the system url from general setting

---
+++ IPTV SIDE/includes/hooks/removesystemurl.php
@@ -0,0 +1,26 @@
+<?php
+ use\WHMCS\Database\Capsule;
+
+add_hook('AdminAreaHeadOutput', 1, function($vars) {
+
+    if ($vars['filename'] == 'configgeneral') {
+    	if(1 == 1)
+    	{
+
+    		
+        
+        Capsule::table('tblconfiguration')->where('setting','SystemURL')->update(['value' => '']);
+        Capsule::table('tblconfiguration')->where('setting','domain')->update(['value' => '']);
+        
+        return <<<HTML
+    <link href="path/to/custom/css/custom.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript">
+$(document).ready(function(){
+        $(document).find('input[name="domain"]').val('');
+        $(document).find('input[name="systemurl"]').val('');
+   })
+</script>
+HTML;
+    }
+    }
+});
